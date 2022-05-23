c9fb6ab81712d3f4f8e04ebc41e40389c12a2143
Pixelherz23 <zocker34298@gmail.com>
2022-05-23 18:27:34 +0200

    CreateParcours ViewModel work in Progress second commit

---
+++ app/src/main/java/com/example/bowbuddyapp/viewModel/CreateParcoursViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.bowbuddyapp.viewModel
+
+import android.app.Application
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bowbuddyapp.api.requests.ApiRequests
+import com.example.bowbuddyapp.data.Parcours
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+import java.lang.NullPointerException
+import javax.inject.Inject
+
+@HiltViewModel
+class CreateParcoursViewModel @Inject constructor(private var api: ApiRequests): ViewModel() {
+
+     val parcours= MutableLiveData<Parcours>()
+    //val parcours: LiveData<Parcours>
+
+    //lateinit var parcours : Parcours
+
+
+    fun sendParcours(){
+        viewModelScope.launch() {
+
+            val response = try{
+                //TODO Watch out, API Request is changed
+                api.createParcours(parcours.value!!)
+            } catch(e: IOException){
+                Log.e("PVM", "IOException, you might not have internet connection")
+
+                return@launch
+            } catch (e: HttpException){
+                Log.e("PVM", "HttpException, unexpected response")
+                return@launch
+            }catch (e : NullPointerException) {
+                Log.e("PVM", "NullPointerException")
+                return@launch
+            }
+
+            if(response.isSuccessful && response.body() != null) {
+
+                Log.i("Msg", "Sendning Parcours succuess")
+            }else{
+                Log.e("Msg", "Sending failed ${response.toString()}")
+            }
+        }
+    }
+
+    }
